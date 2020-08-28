e30e15f2c4122991c2ed0dce71ec85e3afec9106
gnogun <gnogun@gmail.com>
2020-08-28 15:14:01 +0900

    추가 파일 인덱스 추가해서 다시 커밋

---
+++ cashexchange/src/main/java/com/hourfun/cashexchange/interceptor/AuthInterceptor.java
@@ -0,0 +1,28 @@
+package com.hourfun.cashexchange.interceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		if(session != null) {
+			Cookie cookie = new Cookie("id", session.getId());
+			cookie.setPath("/");
+			cookie.setMaxAge(60 * 60 * 24 * 7);
+			response.addCookie(cookie);
+		}
+		
+		
+		
+		return true;
+	}
+}
