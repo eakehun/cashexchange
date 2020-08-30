c32d349e55d01e65f0a44c7c4b264162e4b2e065
gnogun <gnogun@gmail.com>
2020-08-31 01:34:39 +0900

    유저 컨트롤러 리퀘스트 리스폰스 수정중

--- cashexchange/src/main/java/com/hourfun/cashexchange/controller/UsersController.java
+++ cashexchange/src/main/java/com/hourfun/cashexchange/controller/UsersController.java
@@ -1,10 +1,14 @@
 package com.hourfun.cashexchange.controller;
 
 import javax.servlet.http.HttpSession;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,21 +18,26 @@ import com.hourfun.cashexchange.model.UsersRequest;
 import com.hourfun.cashexchange.service.UsersService;
 
 @RestController
+@RequestMapping("/users")
 public class UsersController {
 	
 	@Autowired
 	private UsersService service;
 
-	@PostMapping("/login")
-	public String login(@RequestBody LoginRequest loginRequest, HttpSession session) {
+	@RequestMapping(value ="/login/id/{id}/pwd/{pwd}/", method = RequestMethod.GET)
+	public String login(@PathVariable String id, @PathVariable String pwd, HttpSession session) {
         
-		return service.customLogin(loginRequest, session);
+//		return service.customLogin(loginRequest, session);
+		
+		return "";
 	}
 	
-	@PostMapping("/findid")
-	public @ResponseBody UsersRequest findId(@RequestBody UsersRequest request) {
+	@RequestMapping(value ="/findId/{email}/", method = RequestMethod.GET)
+	public @ResponseBody Users findId(@PathVariable String email) {
 		
-		return service.findId(request);
+//		return service.findId(request);
+		
+		return null;
 	}
 	
 	@PostMapping("/findPassword")
--- cashexchange/src/main/java/com/hourfun/cashexchange/interceptor/AuthInterceptor.java
+++ cashexchange/src/main/java/com/hourfun/cashexchange/interceptor/AuthInterceptor.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AuthInterceptor extends HandlerInterceptorAdapter{
@@ -24,5 +25,12 @@ public class AuthInterceptor extends HandlerInterceptorAdapter{
 		
 		
 		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
 	}
 }
