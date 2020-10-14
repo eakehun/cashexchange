dfc0951c16062ca623e81a3e9221790d5975ba54
gnogun <gnogun@gmail.com>
2020-10-14 15:05:16 +0900

    okcer3 추가

---
+++ cashexchange/src/main/java/com/hourfun/cashexchange/service/UserVerifyService.java
@@ -0,0 +1,32 @@
+package com.hourfun.cashexchange.service;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import kcb.module.v3.OkCert;
+import kcb.module.v3.exception.OkCertException;
+
+public class UserVerifyService {
+	
+	@Value("${okcert.target}")
+	private String target;
+	
+	public void verify() {
+		String cpCd = "V47630000000";
+		String svcName = "IDS_HS_POPUP_START";
+		
+		String license = "";
+		String param = "";
+		
+		OkCert cert = new OkCert();
+		
+		
+		
+		try {
+			cert.callOkCert(target, cpCd, svcName, license, param);
+		} catch (OkCertException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
